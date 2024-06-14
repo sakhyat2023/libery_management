64113d245243b4a1e9394ee939d1a6c7c1c821f6
sakhyat2023 <sakhyat2023@gmail.com>
2024-06-14 13:14:40 +0600

    updated

--- library_projects/settings.py
+++ library_projects/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
